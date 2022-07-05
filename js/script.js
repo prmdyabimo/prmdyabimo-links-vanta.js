a454d2604c97d04dc98af7b48228b0879da0ae5a
prmdyabimo <pramudyabimo1920@gmail.com>
2022-07-05 23:33:44 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+  VANTA.BIRDS({
+    // id
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    // agar menggunakan background dari kita
+    backgroundAlpha: 0.0,
+  })
+})
